bdbe52fb2517a3bba08028d26251ab4f6ad1c8d1
Shaun <splittingred@gmail.com>
2018-01-09 12:06:38 -0600

    Make users able to be load from env

--- app/models/bgg/username_map.rb
+++ app/models/bgg/username_map.rb
@@ -19,16 +19,11 @@ module Bgg
 
     private
 
+    # rubocop disable:all
     def load
-      return unless file_exists?
+      return unless file_exists? || ENV.fetch('USERNAME_MAP', '').to_s.present?
 
-      users = {}
-      data = YAML.load(File.read(file_path)).deep_symbolize_keys
-      data[:users].each do |u|
-        ux = u.split(':')
-        users[ux[0].to_sym] = ux[1]
-      end
-      users
+      file_exists? ? load_from_file : load_from_env
     end
 
     def file_exists?
@@ -37,6 +32,27 @@ module Bgg
 
     def file_path
       "#{Rails.root}/config/users.yml"
+    end
+
+    def load_from_env
+      users = {}
+      map = ENV.fetch('USERNAME_MAP', '').to_s
+      map = map.split(',')
+      map.each do |p|
+        u = p.split(':')
+        users[u[0].to_sym] = u[1]
+      end
+      users
+    end
+
+    def load_from_file
+      users = {}
+      YAML.load(File.read(file_path)).deep_symbolize_keys
+      data[:users].each do |u|
+        ux = u.split(':')
+        users[ux[0].to_sym] = ux[1]
+      end
+      users
     end
   end
 end
