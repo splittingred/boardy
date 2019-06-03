758dbb2e0da066562918a140303eb5b329b7a8f7
Shaun McCormick <splittingred@gmail.com>
2019-06-03 09:04:32 -0500

    Cleanups

--- app/domains/games/suggestions/service.rb
+++ app/domains/games/suggestions/service.rb
@@ -4,10 +4,24 @@ module Games
     # For game suggestions
     #
     class Service
-      def search(player_count: 0, ranked: false, played: nil, random: false, min_time: 0, max_time: 0, users: [], limit: 100)
+      ##
+      # Suggest a game based on search criteria.
+      #
+      # @param [Integer] player_count (Optional) Within a certain player count
+      # @param [Boolean] ranked (Optional) Only suggest bgg ranked games
+      # @param [Boolean] random (Optional) Randomize selection
+      # @param [Integer] min_time (Optional) Only suggest games where playtime is at least this long
+      # @param [Integer] max_time (Optional) Only suggest games where playtime is at most this long
+      # @param [Boolean] played (Optional) If true, suggest only played games. If false, only unplayed games. If nil, either. Requires users parameter.
+      # @param [Array<Entities::User>] users (Optional) Required with played. Filter played games by these users.
+      # @param [Integer] limit (Optional) Limit to X number of games in the final search. Useful with random.
+      # @return [Entities::Game]
+      #
+      def search(player_count: 0, ranked: false, random: false, min_time: 0, max_time: 0, played: nil, users: [], limit: 100)
         users ||= []
 
-        q = ::Game.distinct.in_player_range(player_count, player_count).owned.limit(limit)
+        q = ::Game.distinct.owned.limit(limit)
+        q = q.in_player_range(player_count, player_count) if player_count.to_i.positive?
         q = q.highest_rank if ranked
         q = q.random if random
         q = q.within_time_range(min_time, max_time) if min_time.positive? || max_time.positive?
